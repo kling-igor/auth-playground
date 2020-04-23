9b0562e24d7df45ae186445d60933066a67f7b49
Igor Kling <kling.igor@gmail.com>
2020-04-23 10:42:25 +0400

    play with cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { Repository, getConnection } from 'typeorm';
 import { AppModule } from './app.module';
 
 import { UserModule } from './user/user.module';
@@ -8,10 +9,22 @@ import { UserEntity } from './user/user.entity';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const userService = app.select(UserModule).get(UserService, { strict: true });
+  const connection = getConnection();
+  const userRepository = connection.getRepository(UserEntity);
+  const users = await userRepository
+    .createQueryBuilder('users')
+    .select()
+    .leftJoinAndSelect('users.roles', 'roles')
+    .getMany();
 
-  const admin: UserEntity = await userService.getUserByLogin('kling-igor@yandex.ru');
-  console.log('ADMIN', admin);
+  // const users = await userRepository.find({ relations: ['roles'] });
+
+  console.table(users, ['id', 'firstName', 'lastName', 'roles']);
+
+  // const userService = app.select(UserModule).get(UserService, { strict: true });
+
+  // const admin: UserEntity = await userService.getUserByLogin('kling-igor@yandex.ru');
+  // console.log('ADMIN', admin);
 
   await app.close();
 }
